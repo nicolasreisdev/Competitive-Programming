c8c619891f6bd43c7829ca6fd2ffb07de7cc6140
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-12-13 20:29:22 -0300

    update

--- aulas333/lista2ED/digitalLogarithm.cpp
+++ aulas333/lista2ED/digitalLogarithm.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()[
 
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+    }
 
 
 
--- aulas333/lista2ED/transformTheExpression.cpp
+++ aulas333/lista2ED/transformTheExpression.cpp
@@ -3,13 +3,24 @@ using namespace std;
 
 int main(){
     int n; cin >> n;
-    string x;
-    vector<char> v;
-    for(int i = 0; i < n;i++){
-        cin >> x;
-        cout << x << endl;
-        x.clear();
+    
+    while(n--){
+        string x, ans; cin >> x;
+        stack<char> s;
+        for(int i = 0; i < x.size();i++){
+
+            if(x[i] == '(')continue;
+            if(x[i] >= 'a' and x[i] <= 'z') ans += x[i];
+            else if(x[i] == ')'){
+                ans += s.top();
+                s.pop();
+            }
+            else s.push(x[i]);
+        }
+        cout << ans << endl;
     }
+    
+
 
     return 0;
 }
