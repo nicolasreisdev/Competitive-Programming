74afb8a429705a9d51eb16ed6788bac1eb2f0bd6
Nicolas Reis <nicolas.e.silva@ufv.br>
2026-03-04 19:30:17 -0300

    update

---
+++ leetcode/arrays/81searchInRotatedSortedArray2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;;
+            if(target == nums[mid]) return true;
+
+            if(nums[l] < nums[mid]){
+                if(nums[l] <= target and target < nums[mid]) r = mid-1;
+                else l = mid+1;
+            }
+            else if(nums[l] > nums[mid]){
+                if(nums[mid] < target && target <= nums[r])l = mid+1;
+                else r = mid-1;
+            }
+            else l++;
+        }
+        return false;
+    }
+};
